75cf2ed35c138939d453cc3290babf0c47c10751
Ferm0494 <ferm0494@gmail.com>
2020-07-28 14:29:35 -0600

    Fix images

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-if (screen.width <= 576){
-   limitFunction()
+console.log(screen);
+if (screen.width <= 576) {
+    limitFunction()
 }
 
 window.addEventListener("resize", limitFunction);
 window.addEventListener("onload", limitFunction);
 
 
-function limitFunction (){
-    if(screen.width <= 576){
-        
+function limitFunction() {
+    if (screen.width <= 576) {
+
         var metaDiv = document.getElementById("meta")
         var name = metaDiv.getElementsByTagName("h1")[0]
         var description = metaDiv.getElementsByTagName("h2")[0]
@@ -18,13 +19,13 @@ function limitFunction (){
         name = document.createElement('h4')
         description = document.createElement('h5')
 
-        description.innerHTML="Im a Full Stack Developer Engineer"
+        description.innerHTML = "Im a Full Stack Developer Engineer"
         name.innerHTML = "My name is Fernando Rivas"
 
-        name.classList.add("text-white","text-center","animate__animated","animate__fadeIn")
+        name.classList.add("text-white", "text-center", "animate__animated", "animate__fadeIn")
         description.classList.add("text-white", "text-center", "animate__animated", "animate__fadeIn", "animate__delay-2s")
         metaDiv.prepend(description)
         metaDiv.prepend(name)
-      
+
     }
 }
